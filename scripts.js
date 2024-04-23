7e8b6fd95b96cdcdd12b4515040881ca1176ab1c
alpzeynep <zeynep.aysenur.alp@gmail.com>
2024-04-23 19:55:35 +0300

    Showing the dictionary meaning of words on the screen

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch('${url}${inpWord}')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        result.innerHTML = ' <div class="word">  <h3>${inpWord}</h3>  <button>  <i class="fas fa-volume-up"></i </button>  </div><div class="details"> <p>pos</p>  <p>/sample/</p></div> <p class="word-meaning">   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, minima! </p><p class="word-example"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, minus?</p> ';
+    });
+}); 
